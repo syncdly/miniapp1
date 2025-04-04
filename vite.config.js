bb09f7a57a51342d9e0349541a47f3e91bb34503
syncdly <146030842+syncdly@users.noreply.github.com>
2025-04-04 09:00:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // Projenin kökü burası
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // Doğrudan kök dizindeki index.html
+    },
+    outDir: 'dist'
+  }
+})
